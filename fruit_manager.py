e97fd8e198bf47626e0eddc91046e5b0bf48570a
Christophe LAMOUCHE <christophe.lamouche@orange.com>
2025-12-09 16:56:35 +0100

    Integration de fonctions de tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -23,18 +23,41 @@ def recolter(invent, fruit, quantite):
     print(f"{fruit} récolté, {quantite} ajoutés à l'inventaire.")
 
 
-def vendre(invent, fruit, quantite):
+def vendre(invent, fruit, quantite, treso):
     vendus = min(invent.get(fruit, 0), quantite)
     invent[fruit] = max(invent.get(fruit, 0) - quantite, 0)
+    treso += vendus
     print(f"{fruit} vendus, {vendus} sortis de l'inventaire.")
+    return invent, treso
+
+
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path, "r", encoding="utf-8") as fichier:
+        tresorerie = json.load(fichier)
+    return tresorerie
+
+
+def ecrire_tresorerie(treso, path="data/tresorerie.txt"):
+    with open(path, "w", encoding="utf-8") as fichier:
+        json.dump(treso, fichier, ensure_ascii=False, indent=4)
+
+
+def afficher_tresorerie(treso):
+    print(f"Trésorerie actuelle : {treso:.2f} €")
 
 
 if __name__ == "__main__":
     inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    afficher_tresorerie(tresorerie)
     afficher_inventaire(inventaire)
+
     recolter(inventaire, "fruit de la passion", 25)
     recolter(inventaire, "bananes", 10)
+
     vendre(inventaire, "mangues", 15)
     vendre(inventaire, "ananas", 70)
+
     afficher_inventaire(inventaire)
     ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
